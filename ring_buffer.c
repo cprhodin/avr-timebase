1d5477e3a0be6c9c84db58b694375440a8e66581
Chris Rhodin <chris@notav8.com>
2014-01-19 02:56:10 -0800

    Make echo optional for ring-buffer.

--- ring_buffer.c
+++ ring_buffer.c
@@ -1,13 +1,7 @@
-#include <stddef.h>
-
-#include "project.h"
-#include "ring_buffer.h"
-
-
 /*
- * simple byte at a time ring buffer
+ * simple byte at a time ring-buffer
  *
- *  each ring buffer is defined by a structure with four elements
+ *  each ring-buffer is defined by a structure with four elements
  *
  *   start - the start address of the buffer
  *   limit - the start address plus the buffer size, one past the end
@@ -20,84 +14,255 @@
  */
 
 
-void rb_init(struct ring_buffer * const rb, uint8_t * p, size_t s)
+#include <stddef.h>
+
+#include "project.h"
+#include "ring_buffer.h"
+
+/*
+ * increment and wrap buffer pointer
+ */
+#define rb_inc_ptr(a,b) do {                                                   \
+    b++;                                                                       \
+    if (b == (a)->limit) b = (a)->start;                                       \
+} while (0)
+
+/*
+ * (un)wrap and decrement buffer pointer
+ */
+#define rb_dec_ptr(a,b) do {                                                   \
+    if (b == (a)->start) b = (a)->limit;                                       \
+    b--;                                                                       \
+} while (0)
+
+/*
+ * set ring-buffer state
+ */
+#define set_cantput(a) ((a)->flags |= RB_CANTPUT)
+#define clr_cantput(a) ((a)->flags &= ~RB_CANTPUT)
+#define set_cantget(a) ((a)->flags |= RB_CANTGET)
+#define clr_cantget(a) ((a)->flags &= ~RB_CANTGET)
+
+#if RING_BUFFER_ECHO != 0
+#define set_cantecho(a) ((a)->flags |= RB_CANTECHO)
+#define clr_cantecho(a) ((a)->flags &= ~RB_CANTECHO)
+#endif
+
+/*
+ * initialize a ring-buffer control structure
+ */
+void rb_init(struct ring_buffer * const rb, uint8_t * const p, size_t s)
 {
     rb->start = p;
     rb->limit = p + s;
-    rb->put = p;
-    rb->get = (uint8_t *) rb; // empty
+    rb_clear(rb);
 }
 
 
 /*
- * returns:
+ * rb_put - ring-buffer put
  *
- *   1 - byte queued, buffer not full
- *   0 - byte queued, buffer becomes full
- *  -1 - byte not queued, buffer full
+ *  If space available, add byte to the tail of the ring-buffer and update the
+ *  control structure.  This API also disables the echo facility.  Use this API
+ *  when echo is not required.
+ *
+ * returns:  1 - byte added, more space available
+ *           0 - byte added, no more space available
+ *          -1 - byte not added, no space available
  */
 int8_t rb_put(struct ring_buffer * const rb, volatile uint8_t const * const b)
 {
-    uint8_t * put = rb->put;
+    uint8_t * put;
 
-    /* return if buffer full */
-    if (put == (uint8_t *) rb)
-        return -1;
+    if (rb_cantput(rb)) return -1;
 
-    /* fix get pointer if buffer empty */
-    if (rb->get == (uint8_t *) rb)
-        rb->get = put;
+#if RING_BUFFER_ECHO != 0
+    /* byte will not be available for echo */
+    set_cantecho(rb);
+#endif
 
-    /* put byte into buffer */
-    *put++ = *b;
+    /* byte will be available for get */
+    clr_cantget(rb);
 
-    /* handle pointer wrap */
-    if (put == rb->limit)
-        put = rb->start;
+    /* add byte, update pointers */
+    put = rb->put;
+    *put = *b;
+    rb_inc_ptr(rb, put);
+#if RING_BUFFER_ECHO != 0
+    rb->echo = rb->put = put;
+#else
+    rb->put = put;
+#endif
 
-    /* identify buffer becomes full */
     if (put == rb->get) {
-        rb->put = (uint8_t *) rb;
+        /* no more space on exit */
+        set_cantput(rb);
         return 0;
     }
 
-    rb->put = put;
+    /* more space on exit */
     return 1;
 }
 
 
 /*
- * returns:
+ * rb_unput - ring-buffer un-put
  *
- *   1 - byte dequeued, buffer not empty
- *   0 - byte dequeued, buffer becomes empty
- *  -1 - byte not dequeued, buffer empty
+ *  If available, return byte from the tail of the ring-buffer and update the
+ *  control structure.
+ *
+ * returns:  1 - byte returned, more available
+ *           0 - byte returned, no more available
+ *          -1 - byte not returned, none available
  */
-int8_t rb_get(struct ring_buffer * const rb, volatile uint8_t * const b)
+int8_t rb_unput(struct ring_buffer * const rb, volatile uint8_t * const b)
 {
-    uint8_t * get = rb->get;
+    uint8_t * put;
 
-    /* handle buffer empty */
-    if (get == (uint8_t *) rb)
-        return -1;
+    if (rb_empty(rb)) return -1;
 
-    /* fix put pointer if buffer full */
-    if (rb->put == (uint8_t *) rb)
-        rb->put = get;
+    /* space will be available for put */
+    clr_cantput(rb);
 
-    /* get byte from buffer */
-    *b = *get++;
+    /* return byte, update pointer */
+    put = rb->put;
+    rb_dec_ptr(rb, put);
+    *b = *put;
+    rb->put = put;
 
-    /* handle pointer wrap */
-    if (get == rb->limit)
-        get = rb->start;
+#if RING_BUFFER_ECHO != 0
+    if (rb_cantecho(rb)) {
+        /* if can't echo on entry, can't echo on exit */
+        rb->echo = put;
+    }
+    else if (rb->echo == put) {
+        /* byte will not be available for echo */
+        set_cantecho(rb);
+    }
+#endif
 
-    /* handle buffer becomes empty */
-    if (get == rb->put) {
-        rb->get = (uint8_t *) rb;
+    if (rb->get == put) {
+        set_cantget(rb);
+        /* no more available on exit */
         return 0;
     }
 
-    rb->get = get;
+    /* more available on exit */
     return 1;
 }
+
+
+/*
+ * rb_get - ring-buffer get
+ *
+ *  If available, return byte from the head of the ring-buffer and update the
+ *  control structure.
+ *
+ * returns:  1 - byte returned, more available
+ *           0 - byte returned, no more available
+ *          -1 - byte not returned, none available
+ */
+int8_t rb_get(struct ring_buffer * const rb, volatile uint8_t * const b)
+{
+    uint8_t * get;
+
+    if (rb_cantget(rb)) return -1;
+
+    /* space will be available for put */
+    clr_cantput(rb);
+
+    /* return byte, update pointer */
+    get = rb->get;
+    *b = *get;
+    rb_inc_ptr(rb, get);
+    rb->get = get;
+
+#if RING_BUFFER_ECHO != 0
+    if (get == rb->echo) {
+#else
+    if (get == rb->put) {
+#endif
+        /* no more available on exit */
+        set_cantget(rb);
+        return 0;
+    }
+
+    /* more available on exit */
+    return 1;
+}
+
+
+#if RING_BUFFER_ECHO != 0
+/*
+ * rb_put_echo - ring-buffer put with echo
+ *
+ *  If space available, add byte to the tail of the ring-buffer and update the
+ *  control structure.  This API also enables the echo facility.  Use this API
+ *  when echo is required.
+ *
+ * returns:  1 - byte added, more space available
+ *           0 - byte added, no more space available
+ *          -1 - byte not added, no space available
+ */
+
+int8_t rb_put_echo(struct ring_buffer * rb, volatile uint8_t const * const b)
+{
+    uint8_t * put;
+
+    if (rb_cantput(rb)) return -1;
+
+    /* byte will be available for echo */
+    clr_cantecho(rb);
+
+    /* add byte, update pointer */
+    put = rb->put;
+    *put = *b;
+    rb_inc_ptr(rb, put);
+    rb->put = put;
+
+    if (put == rb->get) {
+        /* no more space on exit */
+        set_cantput(rb);
+        return 0;
+    }
+
+    /* more space on exit */
+    return 1;
+}
+
+
+/*
+ * rb_echo - ring-buffer echo
+ *
+ *  If available, return echo byte from the ring-buffer and update the control
+ *  structure.
+ *
+ * returns:  1 - byte returned, more available
+ *           0 - byte returned, no more available
+ *          -1 - byte not returned, none available
+ */
+int8_t rb_echo(struct ring_buffer * const rb, volatile uint8_t * const b)
+{
+    uint8_t * echo;
+
+    if (rb_cantecho(rb)) return -1;
+
+    /* byte will be available for get */
+    clr_cantget(rb);
+
+    /* return byte, update pointer */
+    echo = rb->echo;
+    *b = *echo;
+    rb_inc_ptr(rb, echo);
+    rb->echo = echo;
+
+    if (echo == rb->put) {
+        /* echo byte not available on exit */
+        set_cantecho(rb);
+        return 0;
+    }
+
+    /* echo byte available on exit */
+    return 1;
+}
+#endif
--- ring_buffer.h
+++ ring_buffer.h
@@ -1,21 +1,77 @@
 #ifndef _RING_BUFFER_H_
 #define _RING_BUFFER_H_
 
+#ifndef RING_BUFFER_ECHO
+#define RING_BUFFER_ECHO 0
+#endif
+
+/*
+ * ring-buffer control stucture
+ */
 struct ring_buffer {
     uint8_t * start;
     uint8_t * limit;
     uint8_t * volatile put;
     uint8_t * volatile get;
+
+#if RING_BUFFER_ECHO != 0
+    uint8_t * volatile echo;
+#endif
+
+    uint8_t flags;
 };
 
+/*
+ * ring-buffer state flags
+ */
+#define RB_CANTPUT 0x01
+#define RB_CANTGET 0x02
 
-#define rb_is_empty(a) ((a)->get == (uint8_t *) (a))
-#define rb_is_full(a) ((a)->put == (uint8_t *) (a))
-#define rb_peek(a) (rb_is_empty(a) ? -1 : (int) *((a)->get))
+#if RING_BUFFER_ECHO != 0
+#define RB_CANTECHO 0x04
+#endif
 
+/*
+ * ring-buffer state tests
+ */
+#define rb_cantput(a) ((a)->flags & RB_CANTPUT)
+#define rb_cantget(a) ((a)->flags & RB_CANTGET)
+#define rb_full(a) rb_cantput(a)
 
-void rb_init(struct ring_buffer * rb, uint8_t * p, size_t s);
-int8_t rb_put(struct ring_buffer * rb, volatile uint8_t const * const b);
-int8_t rb_get(struct ring_buffer * rb, volatile uint8_t * const b);
+#if RING_BUFFER_ECHO != 0
+#define rb_cantecho(a) ((a)->flags & RB_CANTECHO)
+#define rb_empty(a) (rb_cantecho(a) && rb_cantget(a))
+#else
+#define rb_empty(a) rb_cantget(a)
+#endif
+
+/*
+ * clear ring-buffer
+ */
+#if RING_BUFFER_ECHO != 0
+#define rb_clear(a) do {                                                       \
+        (a)->flags = RB_CANTECHO | RB_CANTGET;                                 \
+        (a)->get = (a)->echo = (a)->put = (a)->start;                          \
+    } while (0)
+#else
+#define rb_clear(a) do {                                                       \
+        (a)->flags = RB_CANTGET;                                               \
+        (a)->get = (a)->put = (a)->start;                                      \
+    } while (0)
+#endif
+
+/*
+ * ring-buffer control/access functions
+ */
+extern void rb_init(struct ring_buffer * const rb, uint8_t * const p, size_t s);
+extern int8_t rb_put(struct ring_buffer * rb, volatile uint8_t const * const b);
+extern int8_t rb_unput(struct ring_buffer * rb, volatile uint8_t * const b);
+extern int8_t rb_get(struct ring_buffer * rb, volatile uint8_t * const b);
+
+#if RING_BUFFER_ECHO != 0
+extern int8_t rb_put_echo(struct ring_buffer * rb,
+                          volatile uint8_t const * const b);
+extern int8_t rb_echo(struct ring_buffer * rb, volatile uint8_t * const b);
+#endif
 
 #endif // _RING_BUFFER_H_
